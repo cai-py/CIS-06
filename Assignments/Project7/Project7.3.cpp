b190c06f82ba95cf3af009bf5eace0fbb86ef5ea
Cairo Flores <florescairo2902@gmail.com>
2023-03-21 13:50:39 -0700

    project 7.3

---
+++ Assignments/Project7/Project7.3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 10; i++){
+        cout << "hey" << endl;
+    }
+}
+
+/* 
+This is just a playground to simulate errors to answer the prompt on 7.3
+*/
