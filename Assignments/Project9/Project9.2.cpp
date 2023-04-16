3de651d6963ae67e2a1a897c95b9f7920c6a69f8
Cairo Flores <florescairo2902@gmail.com>
2023-04-16 16:06:33 -0700

    include output

--- Assignments/Project9/Project9.2.cpp
+++ Assignments/Project9/Project9.2.cpp
@@ -13,9 +13,11 @@ int main() {
     int* newArr = doubleArray(arr, size);
 
     // print result
+    cout << "{";
     for (int i = 0; i < size * 2; i++) {
-        cout << newArr[i] << endl;
+        cout << newArr[i] << ", ";
     }
+    cout << "}" << endl;
     
     return 0;
 }
@@ -37,3 +39,8 @@ int* doubleArray(int arr[], int size) {
     // return pointer to new array
     return newArr; // return pointer to new array
 }
+
+/*
+----- OUTPUT -----
+{1, 2, 3, 4, 5, 0, 0, 0, 0, 0, }
+*/
