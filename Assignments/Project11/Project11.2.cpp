3b7543da3c09ba8fd2f026fe5ff66b2b384ba095
Cairo Flores <florescairo2902@gmail.com>
2023-04-19 15:41:55 -0700

    class examples

--- Assignments/Project11/Project11.2.cpp
+++ Assignments/Project11/Project11.2.cpp
@@ -8,33 +8,43 @@ bool paliCheck(string, int);
 int main() {
     string pdString;
 
+    // get user data
     cout << "Enter a word or phrase to check if it is a palindrome: ";
     cin >> pdString;
 
+
     int stringLength = pdString.length();
     if (paliCheck(pdString, stringLength)) {
-        cout << "true";
+        cout << pdString << ", is a palindrome!!" << endl;
     }else {
-        cout << "false";
+        cout << pdString << ", is not a palindrome. :(" << endl;
     }
 
     return 0;
 }
 
 bool paliCheck(string str, int n) {
-    // reverse string 
-    string revStr = "";
-    for (int i = n -1; i >= 0; i--) {
-        revStr += str[i];
-    }
-    
-
-    // compare original string to reverse string
-    // if match return true
-    // if not matched return false
-    if (str == revStr) {
+    // if it loops all the way through the string and all the outer characters match return true
+    if (n <= 1) {
         return true;
-    }else {
+    }
+
+    // check if outer characters dont match
+    if (str[0] != str[n-1]) {
         return false;
     }
+
+    // remove outermost characters and call function again 
+    string newStr = str.substr(1, n-2);
+    int newLength = n - 2;
+    return paliCheck(newStr, newLength);
 }
+
+/*
+----- OUTPUT -----
+Enter a word or phrase to check if it is a palindrome: Computer
+Computer, is not a palindrome. :(
+------------------
+Enter a word or phrase to check if it is a palindrome: level
+level, is a palindrome!!
+*/
--- Class Exercies/RecursionExamples/RecursionExamples.cpp
+++ Class Exercies/RecursionExamples/RecursionExamples.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int fact(int n);
 int factR(int n);
+int binarySearch(int [], int, int);
+int binarySearchR(int [], int, int, int);
 
 int main() {
     int x = 5;
     int y;
+    int arr[] = {5,7,9,11,14,21,23,26,35,44,51,65,71,75,77,81,88,91,95,99};
 
+    cout << "1--> ---------- Factorials ----------" << endl;
     y = fact(x);
     cout << "Fact(" << x << ") = " << y << endl;
 
@@ -16,6 +20,22 @@ int main() {
     y = factR(x);
     cout << "FactR(" << x << ") = " << y << endl;
 
+    cout << "2 --> ---------- Binary Search ----------" << endl;
+    int val = 35;
+    int index = binarySearch(arr, val, 20);
+    if (index >= 0) {
+        cout << val << " found at index " << index << endl;
+    }else {
+        cout << val << " not found" << endl;
+    }
+
+    cout << "2 --> ---------- Recursive Binary Search ----------" << endl;
+    index = binarySearchR(arr, val, 0, 19);
+    if (index >= 0) {
+        cout << val << " found at index " << index << endl;
+    }else {
+        cout << val << " not found" << endl;
+    }
 }
 
 int fact(int n) {
@@ -34,4 +54,36 @@ int factR(int n) {
         result = 1;
     }
     return result;
+}
+
+int binarySearch(int arr[], int val, int n) {
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] > val) {
+            high = mid - 1;
+        }else if (arr[mid] < val) {
+            low = mid + 1;
+        }else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int binarySearchR(int arr[], int val, int start, int end) {
+    if (start > end){
+        return -1;
+    }
+
+    int mid = (start + end) / 2;
+    if (arr[mid] == val) {
+        return mid;
+    }else if(arr[mid] > val) {
+        return binarySearchR(arr, val, start, mid-1);
+    }else {
+        return binarySearchR(arr, val, mid + 1, end);
+    }
 }
