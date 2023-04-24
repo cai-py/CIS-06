f55a501c24fcceb9f9aa64f240e5ed684b70b4b7
Cairo Flores <florescairo2902@gmail.com>
2023-04-24 13:50:14 -0700

    drawn recurssion tree and output added

--- Assignments/Project11/Project11.3.cpp
+++ Assignments/Project11/Project11.3.cpp
@@ -13,8 +13,9 @@ int main() {
 }
 
 int nthFib(int n) {
-    // if not found
+    // this functions n index of the fibonacci sequence
     if (n <= 1) {
+        // if n <= 1 return n / because the first two numbers are 0 and 1
         return n;
     }else {
         return nthFib(n-1) + nthFib(n-2);
@@ -22,3 +23,22 @@ int nthFib(int n) {
 
     return -1;
 }
+
+
+/* OUTPUT
+----------
+Enter the nth number in the fibonacci sequence you want to find: 21
+The 21th fibonacci number is: 10946
+----------
+Enter the nth number in the fibonacci sequence you want to find: 7
+The 7th fibonacci number is: 13
+
+recursion tree nthfib(5)
+
+                    (5)
+                (4)     (3)
+            (3)   (2)  (2)  (1)     
+        (2) (1)                    
+
+((1+1)+1)+(1+1) = 5
+*/
