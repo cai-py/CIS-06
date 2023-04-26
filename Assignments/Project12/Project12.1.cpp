b37474fa39b791993715d3956ddf6cc8106be265
Cairo Flores <florescairo2902@gmail.com>
2023-04-26 14:14:40 -0700

    project prompts

---
+++ Assignments/Project12/Project12.1.cpp
@@ -0,0 +1,14 @@
+/*
+Write a class Sphere with the following properties:
+Private attributes:
+(1) X,Y, Z coordinates of the center
+(2) Radius
+Accessor and mutator methods to
+• Set and get the X, Y, and Z coordinates
+• Set and get the radius
+• Get the volume and surface area of a sphere.
+For a sphere,
+volume = 4Π r3 / 3
+surface area = 4Π r2
+Write a main program to test the Sphere class.
+*/
---
+++ Assignments/Project12/Project12.2.cpp
@@ -0,0 +1,11 @@
+/*
+Write a Person class that contains the following fields and methods:
+• First Name
+• Last Name
+• ID Number
+• Necessary constructors.
+• Methods to return last name, first name, full name, and ID Number
+• Methods to print last name, first name, and ID Number
+Write a main program to test your class.
+
+*/
